9293be321a7c8ec5738d8433c9ffd2b357d258ba
wonsungil <wonsungil@naver.com>
2022-02-09 23:13:42 +0900

    first commit

---
+++ target/debug/Learn_rust.d
@@ -0,0 +1,1 @@
+/Users/wonsungil/dev/repository/study/Learn_rust/target/debug/Learn_rust: /Users/wonsungil/dev/repository/study/Learn_rust/src/main.rs
---
+++ target/debug/deps/Learn_rust-e02376d0423871da.d
@@ -0,0 +1,5 @@
+/Users/wonsungil/dev/repository/study/Learn_rust/target/debug/deps/Learn_rust-e02376d0423871da: src/main.rs
+
+/Users/wonsungil/dev/repository/study/Learn_rust/target/debug/deps/Learn_rust-e02376d0423871da.d: src/main.rs
+
+src/main.rs:
